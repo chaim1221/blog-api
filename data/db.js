f06dbaa7ddbbe88baa929ba06e04d7c0e8fb2f6a
Chaim Eliyah <chaimeliyah@gmail.com>
2018-04-30 12:56:31 -0700

    connected to db

--- data/db.js
+++ data/db.js
@@ -2,9 +2,21 @@
 // config = require("./../config.js");
 
 sqlite3 = require('sqlite3').verbose();
-db = new sqlite3.Database('./../blog.db');
+
+db = new sqlite3.Database('./../blog.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("database connection established");
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("database connection closed");
+});
 
 module.exports = {
-  // code goes here :p
 }
 
