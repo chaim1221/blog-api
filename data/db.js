fd13395c0b7e480226c2ca07b91f2a6761d17f7a
Chaim Eliyah <chaimeliyah@gmail.com>
2018-04-30 12:46:46 -0700

    try to incorporate sql db. not throwing yet.

---
+++ data/db.js
@@ -0,0 +1,10 @@
+// navaja...
+// config = require("./../config.js");
+
+sqlite3 = require('sqlite3').verbose();
+db = new sqlite3.Database('./../blog.db');
+
+module.exports = {
+  // code goes here :p
+}
+
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var databaseTest = require('./data/db.js');
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+router.use(function(request, response, next) {
+    next();
+});
+router.get('/', function(request, response) {
+    response.json({ message: 'test successful (200)' });
+});
+
+
+app.use('/api', router);
+app.listen(port);
+console.log('Listening on port ' + port);
