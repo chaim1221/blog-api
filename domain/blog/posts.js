00c53882f6acf59f276ae9cadd87ffdd7631a5ca
Chaim Eliyah <chaimeliyah@gmail.com>
2018-04-30 21:26:06 -0700

    post_id all the things!

--- domain/blog/posts.js
+++ domain/blog/posts.js
@@ -1,6 +1,6 @@
 // and this is why we don't use plural references:
 var Post = function Post(data) {
-  this.post_id = data.post_id;
+  this.post_id = data.post_id;  // heaven help us
   this.title = data.title;
   this.body = data.body;
 }
--- test/integration/data/blog/posts.spec.js
+++ test/integration/data/blog/posts.spec.js
@@ -1,6 +1,7 @@
 var assert = require('chai').assert;
 var PostsRepository = require(__dirname + '/../../../../data/blog/posts.js').Repository;
 
+// TODO remove post_id from everywhere. srsly
 describe('When we want to keep track of blog posts', function () {
   var postsRepository = new PostsRepository();
   var post = {
@@ -12,21 +13,21 @@ describe('When we want to keep track of blog posts', function () {
     it('Can add blog posts', function (done) {
       postsRepository.add(post).then(function (returnedValue) {
         assert.ok(returnedValue);
-        post.id = parseInt(returnedValue);
-        console.log(post.id);
+        post.post_id = parseInt(returnedValue);
+        //console.log(post.id);
         done();
       });
     });
     it('Can find blog posts by ID', function (done) {
-      postsRepository.findById(post.id).then(function (result) {
-//        //assert.equal(result.id, post.id);
-//        assert.equal(result.title, post.title);
-//        assert.equal(result.body, post.body);
+      postsRepository.findById(post.post_id).then(function (result) {
+        assert.equal(result.post_id, post.post_id);
+        assert.equal(result.title, post.title);
+        assert.equal(result.body, post.body);
         done();
       });
     });
     it('Can remove blog posts', function (done) {
-      postsRepository.remove(post.id).then(function (rowsAffected) {
+      postsRepository.remove(post.post_id).then(function (rowsAffected) {
         assert.equal(rowsAffected, 1);
         done();
       });
